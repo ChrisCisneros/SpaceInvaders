ee905a718ac134c3880d789cca400c2d95370614
ChrisCisneros <chcisneros@csumb.edu>
2021-03-07 22:17:09 -0800

    Full Commit of Part 1
    The first and final commit of the completed part 1 of the Space Invaders Project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  public GameObject bullet;
+    public GameObject player;
+    public Game reference;
+
+  public Transform shottingOffset;
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+        GameObject shot = Instantiate(bullet, shottingOffset.position, Quaternion.identity);
+        //Debug.Log("Bang!");
+
+        Destroy(shot, 6f);
+
+      }
+
+      if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.Translate(-.05f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.Translate(.05f, 0, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        reference.gameOver();
+        Destroy(gameObject);
+    }
+}
